ba5b78dc7bcadaba4b5c49c445be1d18a22554ff
Ale Presacco <pressalex@hotmail.com>
2023-12-07 12:02:16 -0800

    Update Notch_Peak_Filter_Interface.cs
    Adding the option to choose the dB level of the bandwidth

--- Notch_Peak_Filter_Interface.cs
+++ Notch_Peak_Filter_Interface.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace Notch_Peak_Filter
 {
@@ -10,9 +6,11 @@ namespace Notch_Peak_Filter
     {
 
         //Estimate the coeffients of a band-pass filter and return a 2 rows x N coefficients matrix. Row 1 = Numerator; Row 2 = Denumerator
-        double[][] IIRnotch_cpp(double WO, double BW); //Notch filter
+        double[][] IIRnotch_cs(double WO, double BW); //Notch filter
+        double[][] IIRnotch_cs(double WO, double BW, double AB); //Overload of the Notch filter allowing the user to specify the dB level of the bandwidth
 
-        double[][] IIRcomb_cpp(double order, double BW, String type_filt); //Comb or peak filter
+        double[][] IIRcomb_cs(double order, double BW, String type_filt); //Comb or peak filter
+        double[][] IIRcomb_cs(double order, double BW, double AB, String type_filt); //Overload of the Comb or peak filter allowing the user to specify the dB level of the bandwidth
 
         //Filter the data by using the Direct-Form II Transpose, as explained in the Matlab documentation
         double[] Filter_Data_NCP(double[][] coeff_filt, double[] pre_filt_signal);
